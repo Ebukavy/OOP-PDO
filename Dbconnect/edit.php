8b473dc2dd5ae7d3865d43ec77670467a4160cbf
Gideon Tombe <133422903+Ebukavy@users.noreply.github.com>
2023-12-03 04:32:23 +0100

    Create edit.php

---
+++ Dbconnect/edit.php
@@ -0,0 +1,25 @@
+<?php
+include 'db.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $database = new Database();
+    $data = $database->fetchData($id);
+
+
+    if ($data !== false) {
+        // Display form for editing
+        echo "<form action='update.php' method='post'>";
+        echo "<input type='hidden' name='id' value='{$id}'>";
+        echo "Naam: <input type='text' name='Naam' value='{$data['Naam']}'><br>";
+        echo "Leeftijd: <input type='text' name='Leeftijd' value='{$data['Leeftijd']}'><br>";
+        echo "Gender: <input type='text' name='Gender' value='{$data['Gender']}'><br>";
+        echo "<input type='submit' value='Update'>";
+        echo "</form>";
+    } else {
+        echo "Record not found for ID: {$id}";
+    }
+} else {
+    echo "Invalid ID";
+}
+?>
