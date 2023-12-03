748425d29c92a9c7db73a94fb6848c0158372a9a
Gideon Tombe <133422903+Ebukavy@users.noreply.github.com>
2023-12-03 04:32:27 +0100

    Update home.php

--- Dbconnect/home.php
+++ Dbconnect/home.php
@@ -1,9 +1,21 @@
 <?php
 include 'db.php';
 
-
 $database = new Database();
-$database->toevoegenData('Gideon', '70', 'Man');
+$data = $database->fetchData();
 
-echo "Databaseconnectie succesvol tot stand gebracht!";
+echo "<table border='1'>";
+echo "<tr><th>ID</th><th>Naam</th><th>Leeftijd</th><th>Gender</th><th>Action</th></tr>";
+
+foreach ($data as $row) {
+    echo "<tr>";
+    echo "<td>" . (isset($row['id']) ? $row['id'] : '') . "</td>";
+    echo "<td>" . (isset($row['Naam']) ? $row['Naam'] : '') . "</td>";
+    echo "<td>" . (isset($row['Leeftijd']) ? $row['Leeftijd'] : '') . "</td>";
+    echo "<td>" . (isset($row['Gender']) ? $row['Gender'] : '') . "</td>";
+    echo "<td><a href='edit.php?id=" . (isset($row['id']) ? $row['id'] : '') . "'>Edit</a> | <a href='delete.php?id=" . (isset($row['id']) ? $row['id'] : '') . "'>Delete</a></td>";
+    echo "</tr>";
+}
+
+echo "</table>";
 ?>
